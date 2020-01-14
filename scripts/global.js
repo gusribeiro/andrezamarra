c77cbc815d37e9a7b638694a583b9da2aae29109
gustavo.ribeiro <gustavo.ribeiro@opah.com.br>
2020-01-14 10:37:58 -0300

    about

---
+++ scripts/global.js
@@ -0,0 +1,38 @@
+$(function () {
+  // var siteHeader = $('header.site-header');
+  // var range = $(window).outerHeight() / 1;
+
+  // $(window).on('scroll', function () {
+  //   var scrollTop = $(window).scrollTop();
+  //   var calcPos = (scrollTop / range);
+  //   // var calcNeg = 1 - (scrollTop / range);
+
+  //   siteHeader.css({ 'opacity': calcPos });
+
+  //   if (calcPos > 1) {
+  //     siteHeader.css({ 'opacity': '1' });
+  //   } else if (calcPos < 0) {
+  //     siteHeader.css({ 'opacity': '0' });
+  //   }
+  // });
+
+
+
+  // function setPage(target) {
+  //   var page = target || location.hash.substring(1);
+
+  //   $('.container').eq(0).attr('class', 'container ' + (page || 'home'));
+  // }
+  // setPage();
+
+  // $('.nav li a').click(function (e) {
+  //   e.preventDefault();
+  //   console.log(e.target.hash.substring(1));
+  //   // setTimeout(setPage())
+  //   setPage(e.target.hash.substring(1));
+
+  //   // var page = location.hash.substring(1);
+
+  //   // $('.container').attr('class', 'container ' + (page || 'home'));
+  // });
+});
