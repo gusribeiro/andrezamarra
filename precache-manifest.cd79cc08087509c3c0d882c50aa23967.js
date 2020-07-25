2e0de0539cc603354abd5fdb733ae9e45277eea0
Gustavo Ribeiro <git@gustavoribeiro.com>
2020-07-25 20:42:06 -0300

    Updates

---
+++ precache-manifest.cd79cc08087509c3c0d882c50aa23967.js
@@ -0,0 +1,68 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8bbe22ed370552510cf9dece7f8f31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5303b35f7e3380c6b838",
+    "url": "/static/css/2.76cf7f4f.chunk.css"
+  },
+  {
+    "revision": "1cd97bd2fd16ee54f36f",
+    "url": "/static/css/main.53493773.chunk.css"
+  },
+  {
+    "revision": "5303b35f7e3380c6b838",
+    "url": "/static/js/2.a1249839.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a1249839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cd97bd2fd16ee54f36f",
+    "url": "/static/js/main.49df657a.chunk.js"
+  },
+  {
+    "revision": "bf5d04997399e9f46ceb",
+    "url": "/static/js/runtime-main.b8ebd83b.js"
+  },
+  {
+    "url": "/static/media/09f3116d4fd9c65bd6c61655f2b86cfe.09f3116d.ttf"
+  },
+  {
+    "url": "/static/media/09f3116d4fd9c65bd6c61655f2b86cfe.3b8665ef.woff"
+  },
+  {
+    "url": "/static/media/09f3116d4fd9c65bd6c61655f2b86cfe.8427e899.svg"
+  },
+  {
+    "url": "/static/media/09f3116d4fd9c65bd6c61655f2b86cfe.bd9e6778.woff2"
+  },
+  {
+    "url": "/static/media/09f3116d4fd9c65bd6c61655f2b86cfe.e892e245.eot"
+  },
+  {
+    "url": "/static/media/a0e1b1883c0cf520e9c50d0cd91cd0d0.6d21d922.eot"
+  },
+  {
+    "url": "/static/media/a0e1b1883c0cf520e9c50d0cd91cd0d0.7cb5242a.woff2"
+  },
+  {
+    "url": "/static/media/a0e1b1883c0cf520e9c50d0cd91cd0d0.a0e1b188.ttf"
+  },
+  {
+    "url": "/static/media/a0e1b1883c0cf520e9c50d0cd91cd0d0.f672c802.svg"
+  },
+  {
+    "url": "/static/media/a0e1b1883c0cf520e9c50d0cd91cd0d0.fa5c81d6.woff"
+  },
+  {
+    "revision": "97696b7b41df8f4b07bb0d12f9a6f754",
+    "url": "/static/media/background.97696b7b.jpeg"
+  },
+  {
+    "revision": "cbd693e510a1c33e6c8d44848001afe0",
+    "url": "/static/media/instagram.cbd693e5.svg"
+  }
+]);
